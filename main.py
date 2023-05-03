4fa84d749e17e60faf5187c3a3a9fd80c2e1bcea
PabloHernandezUDC <125274113+PabloHernandezUDC@users.noreply.github.com>
2023-05-03 11:38:06 +0200

    moví algunos prints

--- main.py
+++ main.py
@@ -33,19 +33,22 @@ def activity_sum(tree1, tree2):
     '''
     '''
     result_tree = AVL()
-    
+    print('\nProcediendo a crear la suma de actividades...')
+
     for leaf in tree1:
         result_tree[leaf] = tree1[leaf]
     for leaf in tree2:
         if leaf not in tree1 or tree2[leaf] < tree1[leaf]:
             result_tree[leaf] = tree2[leaf]
-        
+    
+    print('La suma de actividades ha sido creada.\n')
     return result_tree
 
 def min_shared_offer(tree1, tree2):
     '''
     '''
     result_tree = AVL()
+    print('\nProcediendo a encontrar la oferta mínima común...')
 
     for leaf in tree1:
         if leaf in tree2:            
@@ -53,7 +56,8 @@ def min_shared_offer(tree1, tree2):
                 result_tree[leaf] = tree1[leaf]
             else:
                 result_tree[leaf] = tree2[leaf]
-
+    
+    print('Ya hemos encontrado la oferta mínima común.\n')
     return result_tree
 
 if __name__ == "__main__":
@@ -62,13 +66,9 @@ if __name__ == "__main__":
     tree_B = create_activity_tree('actividadesB.txt')
     print('Los árboles han sido creados.\n')
     
-    print('Procediendo a crear la suma de actividades...')
     sum_tree = activity_sum(tree_A, tree_B)
-    print('La suma de actividades ha sido creada. Este es el árbol resultante:')
     print_indented_tree(sum_tree, sum_tree.root(), 0)
     
-    print('\nProcediendo a encontrar la oferta mínima común...')
     common_tree = min_shared_offer(tree_A, tree_B)
-    print('Ya hemos encontrado la oferta mínima común. Este es el árbol resultante:')
     print_indented_tree(common_tree, common_tree.root(), 0)
     print('\nFin de la ejecución.\n')
