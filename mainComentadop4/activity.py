9b52ecf6b12ac8513e875a4e704fc376e16570f0
marselo37 <125274181+marselo37@users.noreply.github.com>
2023-05-04 23:27:04 +0200

    Add files via upload

---
+++ mainComentadop4/activity.py
@@ -0,0 +1,139 @@
+# Pablo Hernandez Martinez, pablo.hernandez.martinez@udc.es - Marcelo Ferreiro Sánchez, marcelo.fsanchez@udc.es
+
+class Activity():
+    '''
+    Class that represents an activity that can have a name, duration, number of participants, and cost.
+
+    '''
+    def __init__(self, name, duration, participants, cost):
+        '''
+        Initializes the activity attributes with the values provided as arguments.
+
+        Parameters
+        --------------
+        name str: The name of the activity.
+        duration float: The duration of the activity.
+        participants float: The number of participants in the activity.
+        cost float: The cost of the activity.
+        '''
+        self._name = name
+        try:
+            self._duration = float(duration)
+        except ValueError:
+            self._duration = duration
+        try:
+            self._participants = float(participants)
+        except ValueError:
+            self._participants = participants
+        try:
+            self._cost = float(cost)
+        except ValueError:
+            self._cost = cost
+    
+    def __lt__(self, other):
+        '''
+        Compares two activities and determines which requires fewer resources.
+
+        Parameters
+        ----------
+            other (Activity): The other activity to compare with.
+
+        Returns
+        ----------
+            bool: True if this activity requires fewer resources than the other, false otherwise.
+        '''
+        return self.get_total_needed_resources() < other.get_total_needed_resources()
+    
+    def get_total_needed_resources(self):
+        '''
+        Calculates the amount of resources needed to carry out the activity.
+
+        Returns
+        -----------
+            float: The amount of resources needed, which is defined as the cost divided by the number of participants and duration in hours.
+        '''
+        return self._cost/self._participants/(self._duration/60)
+            
+    def get_name(self):
+        '''
+        Returns the name of the activity.
+
+        Returns
+        --------
+            str: The name of the activity.
+
+        '''
+        return self._name
+    
+    def set_name(self, input_name):
+        '''
+        Sets the name of the activity to the given input.
+
+       Parameters
+       -------------
+            input_name (str): The new name of the activity.
+
+        '''
+        self._name = input_name
+
+    def get_duration(self):
+        '''
+        Returns the duration of the activity.
+
+        Returns
+        -----------
+            float: The duration of the activity.
+        '''
+        return self._duration
+    
+    def set_duration(self, input_duration):
+        '''
+        Sets the duration of the activity to the given input.
+
+        Parameters
+        ----------
+            input_duration float: The new duration for the activity.
+        
+        '''
+        self._duration = input_duration
+
+    def get_participants(self):
+        '''
+        Returns the number of participants in the activity.
+
+        Returns
+        ---------
+            float: The number of participants in the activity.
+        
+        '''
+        return self._participants
+    
+    def set_participants(self, input_participants):
+        '''
+        Sets the number of participants in the activity to the given input.
+
+        Parameters
+        -----------
+            input_participants float: The new number of participants for the activity.
+        '''
+        self._participants = input_participants
+
+    def get_cost(self):
+        '''
+        Returns the cost of the activity.
+
+        Returns
+        ----------
+            float: The cost of the activity.
+        '''
+        return self._cost
+    
+    def set_cost(self, input_cost):
+        '''
+        Sets the number of participants in the activity to the given input.
+
+        Parameters
+        -----------
+            input_cost float: The new cost for the activity.
+        '''
+        self._cost = input_cost
---
+++ mainComentadop4/main.py
@@ -0,0 +1,111 @@
+# Pablo Hernandez Martinez, pablo.hernandez.martinez@udc.es - Marcelo Ferreiro Sánchez, marcelo.fsanchez@udc.es
+from activity import Activity
+from avl_tree import AVL
+import time
+
+def print_indented_activity_tree(T, p, d):
+    '''
+    Taken from avl_tree., slightly modifed
+    This function lets us print any tree with indents representing hierarchy.
+    '''
+    if p is not None:
+        print(4*d*' ' + "(" + str(p.key()) + ",", f'coste: {round(p.value().get_total_needed_resources(), 2)}' + ")") 
+        print_indented_activity_tree(T, T.left(p), d+1)
+        print_indented_activity_tree(T, T.right(p), d+1)
+
+def create_activity_from_line(params):
+    '''
+    Creates an activity object from the list of parameters.
+
+    Parameters
+    ----------
+        params (list): List of parameters of an activity.
+
+    Returns
+    -----------
+        Activity: An activity object created from the list of parameters.
+    '''
+    name, duration, participants, cost = params
+    return Activity(name, duration, participants, cost)
+
+def create_activity_tree(path):
+    '''
+    Creates a tree of activity nodes from a file.
+
+    Parameters
+    -----------
+        path (str): Path of the file containing the activities.
+
+    Returns
+    --------
+        tree: An AVL tree of activity nodes created from the file.
+    '''
+    with open(path) as f:
+        tree = AVL()
+        for element in f.readlines()[1:]: # we skip the first one since its just a header line
+            current_activity = create_activity_from_line(element.split(';'))
+            tree[current_activity.get_name().lower()] = current_activity
+    return tree
+
+def activity_sum(tree1, tree2):
+    '''
+    Calculates the sum of two AVL trees of activity nodes.
+
+    Parameters
+    ----------
+        tree1 (AVL): First AVL tree of activity nodes.
+        tree2 (AVL): Second AVL tree of activity nodes.
+
+    Returns
+    ----------
+        result_tree: An AVL tree of activity nodes containing the sum of the two AVL trees.
+
+    '''
+    result_tree = AVL()
+    print('Procediendo a crear la suma de actividades...')
+
+    for i in tree1:
+        result_tree[i] = tree1[i]
+    for i in tree2:
+        if i not in tree1 or tree2[i] < tree1[i]:
+            result_tree[i] = tree2[i]
+    
+    print('La suma de actividades ha sido creada.\n')
+    return result_tree
+
+def min_shared_offer(tree1, tree2):
+    '''
+    Calculates the minimum shared offer of two AVL trees of activity nodes.
+
+    Parameters:
+        tree1 (AVL): First AVL tree of activity nodes.
+        tree2 (AVL): Second AVL tree of activity nodes.
+
+    Returns:
+       result_tree: An AVL tree of activity nodes containing the minimum shared offer of the two AVL trees.
+    '''
+    result_tree = AVL()
+    print('\nProcediendo a encontrar la oferta mínima común...')
+
+    for i in tree1:
+        if i in tree2:            
+            if tree1[i] < tree2[i]:
+                result_tree[i] = tree1[i]
+            else:
+                result_tree[i] = tree2[i]
+    
+    print('Ya hemos encontrado la oferta mínima común.')
+    return result_tree
+
+if __name__ == "__main__":
+    print('\nIniciando el programa...')
+    tree_A = create_activity_tree('actividadesA.txt')
+    tree_B = create_activity_tree('actividadesB.txt')
+    print('Los árboles han sido creados.\n')
+    
+    tree_C = activity_sum(tree_A, tree_B)
+    print_indented_activity_tree(tree_C, tree_C.root(), 0)
+    
+    tree_C = min_shared_offer(tree_A, tree_B)
+    print_indented_activity_tree(tree_C, tree_C.root(), 0)
+    print('\nFin de la ejecución.\n')
